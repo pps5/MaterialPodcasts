05b042a6fb95a935735108b4922c6ee825199e90
Naoki Nakajima <n2.jima@gmail.com>
2019-01-02 12:24:39 +0900

    merge module for SheetCallbackMediator to MainModule

--- app/src/main/java/io/github/pps5/materialpodcasts/App.kt
+++ app/src/main/java/io/github/pps5/materialpodcasts/App.kt
@@ -3,7 +3,6 @@ package io.github.pps5.materialpodcasts
 import android.app.Application
 import io.github.pps5.materialpodcasts.di.httpModule
 import io.github.pps5.materialpodcasts.di.mainModule
-import io.github.pps5.materialpodcasts.di.sheetCallbackMediatorModule
 import org.koin.android.ext.android.startKoin
 
 class App: Application() {
@@ -11,8 +10,7 @@ class App: Application() {
     companion object {
         private val modules = listOf(
                 httpModule,
-                mainModule,
-                sheetCallbackMediatorModule
+                mainModule
         )
     }
 
--- app/src/main/java/io/github/pps5/materialpodcasts/di/MainModule.kt
+++ app/src/main/java/io/github/pps5/materialpodcasts/di/MainModule.kt
@@ -1,10 +1,12 @@
 package io.github.pps5.materialpodcasts.di
 
+import io.github.pps5.materialpodcasts.view.SheetCallbackMediator
 import io.github.pps5.materialpodcasts.view.viewmodel.TopBarViewModel
 
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 val mainModule = module {
+    single { SheetCallbackMediator() }
     viewModel { TopBarViewModel(get()) }
 }
